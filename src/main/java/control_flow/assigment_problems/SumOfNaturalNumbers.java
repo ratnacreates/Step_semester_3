76206092731f391630f704ced2ff6195c8a03699
ratnacreates <ratnapriya120512@gmail.com>
2026-08-22 16:44:59 +0530

    Add sum of natural numbers

---
+++ src/main/java/control_flow/assigment_problems/SumOfNaturalNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
