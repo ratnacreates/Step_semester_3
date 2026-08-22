fc72b3a539727a2cdf4e66ef93fd55ca260887b3
ratnacreates <ratnapriya120512@gmail.com>
2026-08-22 18:53:15 +0530

    Fix skip multiples of 3

--- src/main/java/control_flow/assigment_problems/SkipMultiplesOf3.java
+++ src/main/java/control_flow/assigment_problems/SkipMultiplesOf3.java
@@ -4,12 +4,7 @@ import java.util.Scanner;
 public class SkipMultiplesOf3 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= 20; i++) {
 
             if (i % 3 == 0) {
                 continue;
@@ -17,7 +12,5 @@ public class SkipMultiplesOf3 {
 
             System.out.println(i);
         }
-
-        sc.close();
     }
 }
