223ddd97da0ed9d1936d648cdb9c449330e13a96
ratnacreates <ratnapriya120512@gmail.com>
2026-08-22 15:36:52 +0530

    Add GCD problem

---
+++ src/main/java/control_flow/class_problems/GCD.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int gcd = 1;
+
+        int limit;
+
+        if (a < b) {
+            limit = a;
+        } else {
+            limit = b;
+        }
+
+        for (int i = 1; i <= limit; i++) {
+
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+
+        System.out.println("GCD = " + gcd);
+
+        sc.close();
+    }
+}
