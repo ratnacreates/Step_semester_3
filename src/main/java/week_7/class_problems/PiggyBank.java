2afd60619280c0231d0d9d4f7dc7787af1770aea
ratnacreates <ratnapriya120512@gmail.com>
2026-09-19 16:08:14 +0530

    Add piggy bank encapsulation

---
+++ src/main/java/week_7/class_problems/PiggyBank.java
@@ -0,0 +1,44 @@
+public class PiggyBank {
+
+    private double savings;
+    private final String id;
+
+    public PiggyBank(String id) {
+        this.id = id;
+        this.savings = 0;
+    }
+
+    public void deposit(double amount) {
+        savings += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > savings) {
+            System.out.println("Withdrawal rejected: insufficient savings");
+        } else {
+            savings -= amount;
+        }
+    }
+
+    public double getSavings() {
+        return savings;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public static void main(String[] args) {
+
+        PiggyBank pb = new PiggyBank("PB-1");
+
+        pb.deposit(100);
+        System.out.println("Savings after deposit: " + pb.getSavings());
+
+        pb.withdraw(30);
+        System.out.println("Savings after withdrawal: " + pb.getSavings());
+
+        pb.withdraw(500);
+        System.out.println("Final savings: " + pb.getSavings());
+    }
+}
