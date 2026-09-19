26dfcd91e2c6e0342d05f19b93e635f475c25497
ratnacreates <ratnapriya120512@gmail.com>
2026-09-19 16:22:57 +0530

    Add playlist defensive copying

---
+++ src/main/java/week_7/assignment_problems/Playlist.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class Playlist {
+
+    private String[] songs;
+    private int songCount;
+
+    public Playlist(int maxSongs) {
+        songs = new String[maxSongs];
+        songCount = 0;
+    }
+
+    public void addSong(String song) {
+        if (songCount < songs.length) {
+            songs[songCount] = song;
+            songCount++;
+        }
+    }
+
+    public String[] getSongs() {
+        return Arrays.copyOf(songs, songCount);
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public static void main(String[] args) {
+
+        Playlist p = new Playlist(10);
+
+        p.addSong("Song A");
+        p.addSong("Song B");
+
+        String[] copy = p.getSongs();
+
+        copy[0] = "Hacked";
+
+        System.out.println("First song in playlist: " + p.getSongs()[0]);
+        System.out.println("Song count: " + p.getSongCount());
+    }
+}
